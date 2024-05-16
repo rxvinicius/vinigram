02cf57e6a3ec3a5853efe76c01f760e0dc6e0262
Vinicius Xavier <vinicius-rodrigues2000@hotmail.com>
2024-05-16 16:37:32 -0300

    feat: Create post

--- src/lib/react-query/queriesAndMutarions.ts
+++ src/lib/react-query/queriesAndMutarions.ts
@@ -1,11 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import {
+  createPost,
   createUserAccount,
+  getRecentPosts,
   signInAccount,
   signOutAccount,
 } from '../appwrite/api';
-import { INewUser } from '@/types';
+import { INewPost, INewUser } from '@/types';
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () =>
   useMutation({
@@ -20,3 +23,22 @@ export const useSignInAccount = () =>
 
 export const useSignOutAccount = () =>
   useMutation({ mutationFn: signOutAccount });
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
+
+export const useGetRecentPosts = () =>
+  useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+  });
