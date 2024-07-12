2da5dd276eaada3ae2a1b3f67ee497510882e5e1
Vinicius Xavier <vinicius-rodrigues2000@hotmail.com>
2024-07-12 20:18:48 -0300

    chore: update no posts message

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -29,7 +29,7 @@ const Saved = () => {
 
       {/* Grid post list */}
       {posts.length === 0 ? (
-        <p className="text-light-4">No available posts</p>
+        <p className="text-light-4">No saved posts</p>
       ) : (
         <ul className="w-full flex justify-center max-w-5xl gap-9">
           <GridPostList posts={posts} showStats={false} showUser={false} />
